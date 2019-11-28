26b1d167894eb58896a1b9dde5c813ddc2d2ca29
K.Hazama <k-hazama@esol.co.jp>
2019-11-28 16:41:04 +0900

    Fixed missing initialization issue(#6)
    In collision_world_distance_field.cpp and
    collision_robot_distance_field.cpp, a collision_detection::Contact
    is created and stored without initalizing the .depth value.
    Since getCollisionSphereCollision() already calculates the distance
    of the objects, returning the penetration depth was added as an
    optional parameter. (The function is not used anywhere else
    other than collision_world_distance_field.cpp and
    collision_robot_distance_field.cpp).

--- moveit_core/collision_distance_field/src/collision_world_distance_field.cpp
+++ moveit_core/collision_distance_field/src/collision_world_distance_field.cpp
@@ -356,15 +356,18 @@ bool CollisionWorldDistanceField::getEnvironmentCollisions(
     if (req.contacts)
     {
       std::vector<unsigned int> colls;
+      std::vector<double> depths;
       bool coll = getCollisionSphereCollision(
           env_distance_field.get(), *collision_spheres_1, *sphere_centers_1, max_propogation_distance_,
-          collision_tolerance_, std::min(req.max_contacts_per_pair, req.max_contacts - res.contact_count), colls);
+          collision_tolerance_, std::min(req.max_contacts_per_pair, req.max_contacts - res.contact_count), colls,
+          &depths);
       if (coll)
       {
         res.collision = true;
         for (unsigned int j = 0; j < colls.size(); j++)
         {
           Contact con;
+          con.depth = depths[j];
           if (is_link)
           {
             con.pos = gsr->link_body_decompositions_[i]->getSphereCenters()[colls[j]];
